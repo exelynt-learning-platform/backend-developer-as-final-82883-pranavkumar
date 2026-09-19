d35e80474a93e14dc3b315b31e48ee6b9c4dd9f2
pranav-i3v <91801619+pranav-i3v@users.noreply.github.com>
2026-09-19 13:12:36 +0530

    Delete src/main directory

--- src/main/java/com/exelynt/booking/common/exception/type/ConflictException.java
+++
@@ -1,9 +0,0 @@
-package com.exelynt.booking.common.exception.type;
-
-/** Maps to HTTP 409: the request is valid but conflicts with the current state. */
-public class ConflictException extends RuntimeException {
-
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/exelynt/booking/resource/common/ResourceType.java
+++
@@ -1,8 +0,0 @@
-package com.exelynt.booking.resource.common;
-
-/** Kind of bookable resource. */
-public enum ResourceType {
-    ROOM,
-    VEHICLE,
-    EQUIPMENT
-}
