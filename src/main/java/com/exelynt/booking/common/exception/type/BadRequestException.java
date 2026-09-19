07a3997be32cefb3ae605095b9c7eb11c038f1b6
pranav-i3v <91801619+pranav-i3v@users.noreply.github.com>
2026-09-19 12:28:43 +0530

    Delete src/main directory

--- src/main/java/com/exelynt/booking/common/exception/type/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.exelynt.booking.common.exception.type;
-
-/** Maps to HTTP 400 for validation failures detected outside Bean Validation. */
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
