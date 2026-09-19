511eda9832840679293078568b4e0a0540bc8df7
pranav-i3v <91801619+pranav-i3v@users.noreply.github.com>
2026-09-19 13:20:43 +0530

    Delete src/main directory

--- src/main/java/com/exelynt/booking/common/exception/type/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.exelynt.booking.common.exception.type;
-
-/** Maps to HTTP 401: credentials or tokens are missing, invalid or no longer usable. */
-public class UnauthorizedException extends RuntimeException {
-
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
